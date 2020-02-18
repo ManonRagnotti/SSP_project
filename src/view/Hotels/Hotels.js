6b0bc892527086959c9e4f528d5be9e846d3dc2a
Manon Ragnotti <manon.ragnotti@hetic.netm>
2020-02-18 13:32:15 +0100

    organize page hotel

--- src/view/Hotels/Hotels.js
+++ src/view/Hotels/Hotels.js
@@ -12,28 +12,29 @@ const Hotels = ({data, res}) => {
 
 
     return (
-      <div className="hotelsContainer">
+      <div className="hotelsPage">
         <ul>
           <li>Paris</li>
           <li>93</li>
           <li>78/95</li>
           <li>92/94</li>
           <li>77/91</li>
-
         </ul>
-        <table>
-          <thead>
-            <tr>
-              <th>Nom</th>
-              <th>Adresse</th>
-              <th>Notation</th>
-              <th>Dernière date de visite</th>
-            </tr>
-          </thead>
 
-          <Carte data={data} />
-
-        </table>
+        <div className="hotelsContainer">
+        <h3>Hotels</h3>
+          <table>
+            <thead>
+              <tr>
+                <th>Nom</th>
+                <th>Adresse</th>
+                <th>Notation</th>
+                <th>Dernière date de visite</th>
+              </tr>
+            </thead>
+            <Carte data={data} />
+          </table>
+        </div>
 
 
       </div>
