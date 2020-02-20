56bd45c96548f7ba9f383d653df9a4d6e510a4b9
Manon Ragnotti <manon.ragnotti@hetic.netm>
2020-02-20 18:27:55 +0100

    Add delete item function and fetch on every pages

--- src/components/VisitorCard/VisitorCard.js
+++ src/components/VisitorCard/VisitorCard.js
@@ -1,13 +1,30 @@
 import React from 'react';
 import './visitorCard.scss';
 
-export default ( {data, v} ) => {
+export default ( {data, v, setRefresh} ) => {
+
+  //DELETE visitor
+  const deleteData = async (_id) => {
+    const res = await fetch('http://localhost:3000/visitor/' + _id, {
+      method: 'DELETE'
+    })
+    await setRefresh(true)
+  }
+
   return (
     <div className="visitor">
       <span className="lastName">{v.lastName}</span>
       <span className="name">{v.name}</span>
       <span className="secteur"> Secteur {v.area}</span>
       <span className="status"> Disponible</span>
+      <div className="visitor-edit">
+        <button className="visitor-edit_btn edit">
+          <img alt="" className="icon-pen" src={require('../../assets/img/pen.svg')}></img>
+        </button>
+        <button className="visitor-edit_btn delete" onClick={() => deleteData(v._id)}>
+          <img alt="" className="icon-trash" src={require('../../assets/img/trash.svg')}></img>
+        </button>
+      </div>
     </div>
   )
 }
