0a846639bb87650bc332de4d7341f17e8d9361ca
Eugenie Roques <eroques@OBFR-eroques.local>
2020-02-20 13:41:33 +0100

    added drag and drop planning

--- src/components/Calendar/Calendar.js
+++ src/components/Calendar/Calendar.js
@@ -10,16 +10,31 @@ import interactionPlugin, { Draggable } from '@fullcalendar/interaction';
 
 
 export default function Calendar() {
-   
+    useEffect(() => {
+        let draggableEl = document.getElementById("external-events");
+        new Draggable(draggableEl, {
+            itemSelector: ".fc-event",
+            eventData: function(eventEl) {
+              let title = eventEl.getAttribute("title");
+              let id = eventEl.getAttribute("data");
+              return {
+                title: title,
+                id: id
+              };
+            }
+          })
+      }, [])
     return(
         <div>
-
         <Fullcalendar 
             defaultView="timeGridWeek"
             plugins={[ timeGridPlugin, interactionPlugin ]}
             droppable={true}
             weekends={false}
             locale="fr"
+            events={[{id: 'a',
+            title: 'my event',
+            start: '2020-02-20'}]}
         />
         </div>
     )
