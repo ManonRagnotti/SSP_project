173255510b8d1b2766528adf585f85ad38cf6f88
Eugenie Roques <eroques@OBFR-eroques.local>
2020-02-20 18:22:24 +0100

    fetch jwt token login

--- src/view/Login/Login.js
+++ src/view/Login/Login.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React,{ useState } from 'react';
 import './Login.scss';
 import "antd/dist/antd.css";
-import { Switch } from 'antd';
+import {useHistory} from 'react-router-dom'
+import { getFileItem } from 'antd/lib/upload/utils';
 
-const Login = ({values, setValues, handleSubmit}) => {
-  const {email,password} = values
+const Login = ({ login }) => {
+  const [ email, setEmail ] = useState('')
+  const [ password, setPassword ] = useState('')
+  const history = useHistory()
+
+  console.log(setEmail, setPassword)
+  const handleSubmit = (e)=>{
+      e.preventDefault();
+      login(email, password)
+    history.push('/planning')
+
+  }
+
     return(
         <section className="login">
             <div className="login-left">
@@ -13,18 +25,18 @@ const Login = ({values, setValues, handleSubmit}) => {
                 </div>
             </div>
             <div className="login-right">
-                <h1>Vous êtes :</h1>
-                <div className="login-switch">
+                <h1>Connectez-vous :</h1>
+                {/* <div className="login-switch">
                     <span>Planificateur</span> <Switch className="login-switch-btn" defaultChecked /> <span>Intervenant</span>
-                </div>
-                <form className="login-form">
+                </div> */}
+                <form className="login-form" onSubmit={handleSubmit}>
                     <input
                         type="text"
                         name="first_name"
                         className="login-form_input mail"
                         placeholder="Adresse email"
                         value={email}
-                        onChange={e=> setValues({...values,email: e.target.value})}
+                        onChange={e => setEmail(e.target.value)}
                         required
                     />
                     <input
@@ -33,10 +45,10 @@ const Login = ({values, setValues, handleSubmit}) => {
                         className="login-form_input password"
                         placeholder="Mot de passe"
                         value={password}
-                        onChange={e=> setValues({...values,password: e.target.value})}
+                        onChange={e=> setPassword(e.target.value)}
                         required
                         />
-                    <input type="submit" className="login-form_submit" value="Valider" onClick={handleSubmit()}/>
+                    <input type="submit" className="login-form_submit" value="Valider" />
                 </form>
             </div>
         </section>
--- src/view/Login/LoginContainer.js
+++ src/view/Login/LoginContainer.js
@@ -1,41 +1,29 @@
 import React, {useState} from 'react';
 import Login from "./Login";
+
+
 const  LoginContainer =()=> {
-  const initialValue = {
-    first_name: '',
-    last_name: '',
-    email: '',
-    password: '',
-  }
-  const [values, setValues]= useState(initialValue)
-//   const handleSubmit = e => {
-//     e.preventDefault()
-//     console.log(values)
-//   }
-  const handleSubmit = e => {
-    e.preventDefault();
-    this.props.form.validateFields((err, values) => {
-      if (!err) {
-        console.log('Received values of form: ', values);
-      }
+  
+  const [logged, setLogged] = useState(false);
+
+  const login = (email, password) => {
+    fetch("http://localhost:3000/user/login", {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ email, password })
+    })
+    .then((response) => {
+      response.json().then((result)=> {
+        console.log(result);
+        localStorage.setItem('token', result.token)  
+      })
     });
-  };
+  }
 
-  const [body, setBody] = useState();
 
-  fetch("", {
-    method: "post",
-    headers: {
-      'Accept' : 'application/json',
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(setBody)
-  })
-  .then((response) => {
-    console.log(response);
-  });
+
   return (
-          <Login values={values} setValues={setValues} handleSubmit={handleSubmit}/>
+          <Login login={login} />
   )
 }
 export default LoginContainer
